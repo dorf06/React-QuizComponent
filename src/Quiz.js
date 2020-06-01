7cbac3b6578b196a21aa148240bae88254a6ff41
Wagendorf <cordell.wagendorf@ndus.edu>
2020-06-01 16:16:29 -0700

    Module 7 Done

--- src/Quiz.js
+++ src/Quiz.js
@@ -16,7 +16,15 @@ class Quiz extends Component {
 
     showNextQuestion () {
         
-        this.setState({quiz_position: this.state.quiz_position + 1});
+        this.setState({
+            quiz_position: this.state.quiz_position + 1
+        });
+    }
+
+    handleResetClick () {
+        this.setState({
+            quiz_position: 1
+        })
     }
     
     render() {
@@ -25,7 +33,7 @@ class Quiz extends Component {
         return (
             <div>
                 { isQuizEnd 
-                    ? <QuizEnd /> 
+                    ? <QuizEnd resetClickHandler={this.handleResetClick.bind(this)}/> 
                     : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} showNextQuestionHandler={this.showNextQuestion.bind(this)} /> 
                 }
             </div>
